bd589b14330f04b0423fdba4e93fc30de3532c23
soulwine-it <skyorwind@gmail.com>
2020-04-02 18:21:03 +0900

    React map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
 
+function Food({ name, age }) {
+  return (
+    <div>
+      <div>I like {name}</div>
+      <div>I like {age}</div>
+    </div>
+  );
+}
+const foodILike = [
+  {
+    name: "kimchi",
+    age: "3"
+  },
+  {
+    name: "jjajangmyeon",
+    age: 5
+  },
+  {
+    name: "dongas",
+    age: 10
+  }
+];
+
 function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      {foodILike.map(dish => (
+        <Food name={dish.name} age={dish.age} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
