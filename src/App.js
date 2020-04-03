12b8ffc9162d1e83dc6fa4cc69a4d163a94e6f54
soulwine-it <skyorwind@gmail.com>
2020-04-03 15:36:46 +0900

    map Recap test

--- src/App.js
+++ src/App.js
@@ -1,34 +1,41 @@
 import React from "react";
 
-function Food({ name, age }) {
-  return (
-    <div>
-      <div>I like {name}</div>
-      <div>I like {age}</div>
-    </div>
-  );
-}
 const foodILike = [
   {
+    id: 1,
     name: "kimchi",
     age: "3"
   },
   {
+    id: 2,
     name: "jjajangmyeon",
     age: 5
   },
   {
+    id: 3,
     name: "dongas",
     age: 10
   }
 ];
 
+function Food({ name, age }) {
+  return (
+    <div>
+      <div>I like {name}</div>
+      <div>my age is {age}</div>
+    </div>
+  );
+}
+// function renderFood(dish) {
+//   return <Food name={dish.name} age={dish.age} />;
+// }
 function App() {
   return (
     <div>
       {foodILike.map(dish => (
-        <Food name={dish.name} age={dish.age} />
+        <Food key={dish.id} name={dish.name} age={dish.age} />
       ))}
+      {/* {foodILike.map(renderFood)} */}
     </div>
   );
 }
