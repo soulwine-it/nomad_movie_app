db7ca5e9502c331b31f20febc13d0e7d517c34c8
soulwine-it <skyorwind@gmail.com>
2020-04-07 17:12:35 +0900

    Updates

---
+++ precache-manifest.c589e01394c2d8309d3511a6f14dbf66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96ffee8b0d12e929fcb51812a46dddac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e86f8c03507053c053c",
+    "url": "/static/css/main.5843efd1.chunk.css"
+  },
+  {
+    "revision": "3e965b943082154cfd78",
+    "url": "/static/js/2.e6db0aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6db0aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e86f8c03507053c053c",
+    "url": "/static/js/main.3d8e9c62.chunk.js"
+  },
+  {
+    "revision": "9c93d6aaf2e0932819af",
+    "url": "/static/js/runtime-main.aaa011f9.js"
+  }
+]);
